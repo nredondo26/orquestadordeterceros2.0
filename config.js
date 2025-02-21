d7a2c2b002610d51b9c0ccb336cbff525ba06ec7
Nelson Enrique Redondo Barros <30601611+nredondo26@users.noreply.github.com>
2025-02-21 16:07:15 -0500

    Add files via upload
    configuraciones

---
+++ config.js
@@ -0,0 +1,16 @@
+// config.js
+module.exports = {
+    api: {
+        //produccion
+        //url: 'https://orquestador-pasarelas-gateway-alcance.crediservices.credibanco.com/credibanco/api/pasarelas/orquestador/v1/externo/notificaciones/terminales',
+        //authorization: 'Basic Y3JlZGliYW5jb3BydWViYXM6Q29sb21iaWEyMyo='
+
+        //pruebas
+        url: 'https://orquestador-pasarelas-migracion-pasarelas-pre-prod.apps-pruebas.credibanco.com/credibanco/api/pasarelas/orquestador/v1/externo/notificaciones/terminales',
+        authorization: 'Basic cGFzYXJlbGFzLXVzZXItdGVzdDpDb2xvbWJpYS4yMDI1'
+    },
+    server: {
+        port: 3001,
+        uploadFolder: 'uploads'
+    }
+};
