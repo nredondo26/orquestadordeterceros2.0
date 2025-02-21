9c5dbcabfc5bad2236fd730429b96482ff3e7cf0
Nelson Enrique Redondo Barros <30601611+nredondo26@users.noreply.github.com>
2025-02-21 16:08:33 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,12 @@
 module.exports = {
     api: {
         //produccion
-        //url: 'https://orquestador-pasarelas-gateway-alcance.crediservices.credibanco.com/credibanco/api/pasarelas/orquestador/v1/externo/notificaciones/terminales',
-        //authorization: 'Basic Y3JlZGliYW5jb3BydWViYXM6Q29sb21iaWEyMyo='
+        url: 'https://orquestador-pasarelas-gateway-alcance.crediservices.credibanco.com/credibanco/api/pasarelas/orquestador/v1/externo/notificaciones/terminales',
+        authorization: 'Basic Y3JlZGliYW5jb3BydWViYXM6Q29sb21iaWEyMyo='
 
         //pruebas
-        url: 'https://orquestador-pasarelas-migracion-pasarelas-pre-prod.apps-pruebas.credibanco.com/credibanco/api/pasarelas/orquestador/v1/externo/notificaciones/terminales',
-        authorization: 'Basic cGFzYXJlbGFzLXVzZXItdGVzdDpDb2xvbWJpYS4yMDI1'
+        //url: 'https://orquestador-pasarelas-migracion-pasarelas-pre-prod.apps-pruebas.credibanco.com/credibanco/api/pasarelas/orquestador/v1/externo/notificaciones/terminales',
+        //authorization: 'Basic cGFzYXJlbGFzLXVzZXItdGVzdDpDb2xvbWJpYS4yMDI1'
     },
     server: {
         port: 3001,
